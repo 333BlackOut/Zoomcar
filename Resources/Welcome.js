5f302c69da4a9cdc8dfc7b824c977ed1261086e8
333BlackOut <89317410+333BlackOut@users.noreply.github.com>
2021-12-28 02:14:21 +0530

    js

--- Resources/Welcome.js
+++ Resources/Welcome.js
@@ -1,10 +1,8 @@
-/* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
 function OpenNav() 
 {
     document.getElementById("SideNav").style.width = "500px";
     document.getElementById("SideNav").style.borderRight = "1px dashed black";
 }
-/* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
 function CloseNav() 
 {
   document.getElementById("SideNav").style.width = "0";
